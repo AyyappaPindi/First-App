4138e759f49b71827b720ce92d39817cc5cce637
Ayyappa Pindi <ayyappa.pindi@pyraminds.co>
2016-11-03 13:12:12 +0530

    Code Updates 3/11
    Code Updates 3/11

--- app.js
+++ app.js
@@ -1,65 +1,22 @@
-var arr = [
-    1, 
-    false, 
-    {
-        name: 'Tony',
-        address: 'USA'
-    },
-    function(name) {
-        var greeting = 'Hello ';
-        console.log(greeting + name);
-    }
-];
+function greet(firstname,  lastname, language){
 
-console.log(arr);
-arr[3](arr[2].name);
-
-//by value (primitives)
-
-console.log(this);
-
-var a = 3;
-var b;
-
-b = a;
-a = 2;
-//alert (b);
-//alert(a);
-
-//by reference (all objects (including functions))
-
-var c = { greeting: 'Hi' };
-var d;
-
-d = c;
-
-c.greeting = 'Hello';
-
-console.log(c.greeting);
-console.log(d.greeting);
-
-/*var employee = {
-    firstname: 'Ayyappa',
-    lastname: 'Pindi',
-    address: {
-        street: '4th cross, 4th main',
-        area: 'Banashankari'
-    }
-};
-
-console.log(employee);
-
-function retrieve(data){
-    console.log (data.firstname);
-    console.log (data.lastname);
-    console.log (data.address.area);
+    language = language || 'en';
+    
+    console.log(firstname);
+    console.log(lastname);
+    console.log(language);
+    console.log(arguments);
+    console.log('----------')
 }
 
-retrieve(employee);
+/*function greet(firstname,  lastname, language){
+    
+    console.log(arguments);
+    console.log('----------')
+}*/
 
-retrieve({
-    firstname: "Anitha",
-    lastname: "Pindi",
-    address: "asdasd"
-});
-*/
+greet('Ayyappa', 'Pindi', 'English');
+
+//greet();
+
+//greet('Hello');
